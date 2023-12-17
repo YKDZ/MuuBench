6f663df48d7c9258934bd4f106673bc9a3f20bfe
YKDZ <3070799584@qq.com>
2023-12-17 15:22:08 +0800

    Changes to be committed: 	new file:   docs/intro.md 	modified:   docusaurus.config.js 	modified:   src/pages/index.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: '木制工作站',
+  tagline: '博客 文档',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -20,8 +20,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'YKDZ', // Usually your GitHub org/user name.
+  projectName: 'CraftBench', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
