e69934931a73c650416dcfad9ed148f1d20ae26f
YKDZ <3070799584@qq.com>
2024-01-04 19:06:09 +0800

    Update Record

---
+++ blog/2024/1/4/recollection-develop-notes.md
@@ -0,0 +1,14 @@
+---
+authors:
+  - ykdz
+tags:
+  - Minecraft
+  - Server
+  - Development
+  - Recollection
+---
+
+# Recollection 开发小记
+
+## 一句话概括
+
--- src/theme/Footer/Record/index.js
+++ src/theme/Footer/Record/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './style.css'; // 引入样式文件
+import './style.css';
 
 function FooterRecord() {
   const additionalLinks = [
@@ -8,17 +8,23 @@ function FooterRecord() {
       href: 'https://beian.miit.gov.cn/',
     },
     {
-      label: '公安备案号',
-      href: '/public-security-beian',
+      label: '粤公网安备 44030002002026 号',
+      href: 'https://beian.mps.gov.cn/#/query/webSearch?code=44030002002026',
+      imgSrc: 'img/beian.png',
     },
   ];
 
   return (
     <div className="footer-record-container">
       {additionalLinks.map((link, index) => (
-        <a key={index} href={link.href} className="footer-record-link">
-          {link.label}
-        </a>
+        <div key={index} className="footer-record-entry">
+          {link.imgSrc && (
+            <img src={link.imgSrc} alt="Image" className="footer-record-image" />
+          )}
+          <a href={link.href} className="footer-record-link">
+            {link.label}
+          </a>
+        </div>
       ))}
     </div>
   );
